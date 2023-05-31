29c9f7dc964380847bbc66973ab7d418c98b0601
linjialiang <linjialiang@163.com>
2023-05-31 20:17:36 +0800

    update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -6,7 +6,7 @@ import sidebar from "./sidebar/main";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: "/",
-  lang: "zh-CN",
+  lang: "zh",
   title: "PHP Environment",
   description: "基于Debian的php环境搭建教程",
   lastUpdated: true,
@@ -20,29 +20,7 @@ export default defineConfig({
       pattern: "https://github.com/linjialiang/php-environment/main/:path",
       text: "在 github 上编辑",
     },
-    search: {
-      provider: "local",
-      options: {
-        locales: {
-          zh: {
-            translations: {
-              button: {
-                buttonText: "搜索文档",
-                buttonAriaLabel: "搜索文档",
-              },
-              modal: {
-                noResultsText: "无法找到相关结果",
-                resetButtonTitle: "清除查询条件",
-                footer: {
-                  selectText: "选择",
-                  navigateText: "切换",
-                },
-              },
-            },
-          },
-        },
-      },
-    },
+    search: { provider: "local" },
     nav: [
       { text: "主页", link: "/" },
       { text: "环境搭建", items: environmentNav },
