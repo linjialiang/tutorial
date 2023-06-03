ec19de6ca006034f9be46d586c6d1884a4d8a160
linjialiang <linjialiang@163.com>
2023-06-03 15:19:48 +0800

    update

--- .vitepress/sidebar/other/vitepress.ts
+++ .vitepress/sidebar/other/vitepress.ts
@@ -6,7 +6,7 @@ const sidebar = [
   { text: '概述', link: `${path.main}` },
   { text: '快速开始', link: `${path.main}quickstart` },
   { text: '路由', link: `${path.main}route` },
-  { text: 'MarkDown扩展', link: `${path.main}md` },
+  { text: 'MarkDown', link: `${path.main}md` },
   { text: '配置', link: `${path.main}config` },
   { text: '部署指南', link: `${path.main}deploy` },
 ];
