0f618e23030f231aa31d816c265a51c46bfd2285
linjialiang <linjialiang@163.com>
2023-06-15 20:55:26 +0800

    update

--- .vitepress/sidebar/environment/main.ts
+++ .vitepress/sidebar/environment/main.ts
@@ -1,22 +1,28 @@
 const path = {
-  main: "/environment/",
+  main: '/environment/',
+  k8sMain: '/environment/k8s/',
 };
 
 const sidebar = [
-  { text: "概述", link: `${path.main}` },
-  { text: "Nginx", link: `${path.main}nginx` },
+  { text: '概述', link: `${path.main}` },
+  { text: 'Nginx', link: `${path.main}nginx` },
   {
-    text: "SQL",
+    text: 'SQL',
     collapsed: true,
     items: [
-      { text: "MySQL", link: `${path.main}mysql` },
-      { text: "Sqlite3", link: `${path.main}sqlite3` },
-      { text: "PostgreSQL", link: `${path.main}pgsql` },
+      { text: 'MySQL', link: `${path.main}mysql` },
+      { text: 'Sqlite3', link: `${path.main}sqlite3` },
+      { text: 'PostgreSQL', link: `${path.main}pgsql` },
     ],
   },
-  { text: "Redis", link: `${path.main}redis` },
-  { text: "MongoDB", link: `${path.main}mongodb` },
-  { text: "PHP", link: `${path.main}php` },
+  { text: 'Redis', link: `${path.main}redis` },
+  { text: 'MongoDB', link: `${path.main}mongodb` },
+  { text: 'PHP', link: `${path.main}php` },
+  {
+    text: 'K8S系列',
+    collapsed: true,
+    items: [{ text: '概述', link: `${path.k8sMain}` }],
+  },
 ];
 
 export default sidebar;
