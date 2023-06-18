891f7c9abfd31a4ea6f7137fa747077e0d6f1704
linjialiang <linjialiang@163.com>
2023-06-18 19:37:29 +0800

    update

--- .vitepress/sidebar/docker/main.ts
+++ .vitepress/sidebar/docker/main.ts
@@ -5,6 +5,7 @@ const path = {
 const sidebar = [
   { text: '概述', link: `${path.main}` },
   { text: '安装docker', link: `${path.main}install` },
+  { text: '在 Docker 中管理数据', link: `${path.main}storage` },
 ];
 
 export default sidebar;
---
+++ docker/storage.md
@@ -0,0 +1,6 @@
+---
+title: 在 Docker 中管理数据
+titleTemplate: Docker 教程
+---
+
+# {{ $frontmatter.title }}
