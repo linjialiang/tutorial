4c1eb8e2824ebf20baa369c4c73910dbcc0300af
linjialiang <linjialiang@163.com>
2023-06-19 22:30:24 +0800

    update

--- .vitepress/sidebar/docker/main.ts
+++ .vitepress/sidebar/docker/main.ts
@@ -6,6 +6,7 @@ const sidebar = [
   { text: '概述', link: `${path.main}` },
   { text: '安装docker', link: `${path.main}install` },
   { text: '在 Docker 中管理数据', link: `${path.main}storage` },
+  { text: '使用 Volumes 持久存储', link: `${path.main}volumes` },
 ];
 
 export default sidebar;
---
+++ docker/volumes.md
@@ -0,0 +1,6 @@
+---
+title: 使用 Volumes 持久存储
+titleTemplate: Docker 教程
+---
+
+# {{ $frontmatter.title }}
