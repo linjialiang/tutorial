566c76488ca36917a49c3b5cc1be9af79e67abeb
linjialiang <linjialiang@163.com>
2023-06-19 20:01:47 +0800

    update

--- .vitepress/nav/other.ts
+++ .vitepress/nav/other.ts
@@ -3,6 +3,7 @@ const nav = [
   { text: '概述', link: '/other/' },
   { text: 'vitepress', link: '/other/vitepress/' },
   { text: 'vscode', link: '/other/vscode/' },
+  { text: 'clash', link: '/other/clash/' },
 ];
 
 export default nav;
