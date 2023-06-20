97f493ea12686a20438548dde0088d3a4db419d2
linjialiang <linjialiang@163.com>
2023-06-21 00:08:45 +0800

    update

--- .vitepress/sidebar/docker/main.ts
+++ .vitepress/sidebar/docker/main.ts
@@ -9,6 +9,7 @@ const sidebar = [
   { text: '使用 卷 持久存储', link: `${path.main}volumes` },
   { text: '使用 绑定挂载 持久存储', link: `${path.main}bind_mounts` },
   { text: '使用 tmpfs 临时存储', link: `${path.main}tmpfs_mounts` },
+  { text: '排查卷错误', link: `${path.main}troubleshoot` },
 ];
 
 export default sidebar;
---
+++ docker/troubleshoot.md
@@ -0,0 +1,6 @@
+---
+title: 排查卷错误
+titleTemplate: Docker 教程
+---
+
+# {{ $frontmatter.title }}
