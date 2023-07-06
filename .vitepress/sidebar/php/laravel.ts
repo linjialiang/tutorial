a1d24b19b32337549181f6310448fa6afaa0b7d9
linjialiang <linjialiang@163.com>
2023-07-06 22:01:32 +0800

    update

--- .vitepress/sidebar/php/laravel.ts
+++ .vitepress/sidebar/php/laravel.ts
@@ -2,6 +2,9 @@ const path = {
   main: '/php/laravel/',
 };
 
-const sidebar = [{ text: '概述', link: `${path.main}` }];
+const sidebar = [
+  { text: '概述', link: `${path.main}` },
+  { text: '让laravel运行', link: `${path.main}start` },
+];
 
 export default sidebar;
