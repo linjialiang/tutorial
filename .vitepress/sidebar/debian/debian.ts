cbd7581d767cc7b4dd3ebd408ff49817cc03d33d
linjialiang <linjialiang@163.com>
2023-07-13 00:24:44 +0800

    update

--- .vitepress/sidebar/debian/debian.ts
+++ .vitepress/sidebar/debian/debian.ts
@@ -6,6 +6,7 @@ const sidebar = [
   { text: '概述', link: `${path.main}` },
   { text: '安装操作系统', link: `${path.main}install_os` },
   { text: '配置操作系统', link: `${path.main}config_os` },
+  { text: 'xfce桌面', link: `${path.main}xfce` },
 ];
 
 export default sidebar;
---
+++ debian/xfce.md
@@ -0,0 +1,10 @@
+---
+title: Xfce桌面
+titleTemplate: Debian 教程
+---
+
+# {{ $frontmatter.title }}
+
+## 桌面软件安装
+
+通常 Xfce、Gnome 桌面的软件安装方式是一致的
