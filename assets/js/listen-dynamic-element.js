bc8c984ebfd646df706fb37400d077c89e04fa3c
linjialiang <linjialiang@163.com>
2023-07-17 20:27:36 +0800

    update

---
+++ assets/js/listen-dynamic-element.js
@@ -0,0 +1,18 @@
+/**
+ * js监听动态生成的元素 - 事件委托
+ * @param {String} eventType js事件类型 click、keyup等
+ * @param {String} element 非动态生成的父级元素
+ * @param {String} selector 当前需要监听的动态元素
+ * @param {Object} fn 函数
+ */
+function on(eventType, element, selector, fn) {
+  if (!(element instanceof Element)) {
+    element = document.querySelector(element);
+  }
+  element.addEventListener(eventType, (e) => {
+    const t = e.target;
+    if (t.matches(selector)) {
+      fn(e);
+    }
+  });
+}
--- js/index.md
+++ js/index.md
@@ -3,3 +3,7 @@
 ::: details 防抖函数
 <<<@/assets/js/debounce.js
 :::
+
+::: details 监听动态生成的元素
+<<<@/assets/js/listen-dynamic-element.js
+:::
