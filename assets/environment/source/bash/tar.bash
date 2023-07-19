a3132f364d32171bea39a1f57a35aa1b931bb7cf
linjialiang <linjialiang@163.com>
2023-07-19 18:45:29 +0800

    update

--- assets/environment/source/bash/tar.bash
+++ assets/environment/source/bash/tar.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # 设定压缩文件根目录
-TAR_DIR = '~/debian/environment/'
+TAR_DIR="/home/emad/debian/environment"
 # 设置解压路径根目录
-PACKAGE_DIR = '/package'
+PACKAGE_DIR="/package"
 
 # 进入压缩文件根目录
 cd ${TAR_DIR}
@@ -18,12 +18,6 @@ for i in `ls *.tar.bz2`;
 do
     echo "bz2解压" $i
     tar -xjf $i -C ${PACKAGE_DIR}
-done
-
-for i in `ls *.tar.xz`;
-do
-    echo "xz解压" $i
-    tar -xJf $i -C ${PACKAGE_DIR}
 done
 
 # 进入php目录
