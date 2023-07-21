786c225a2082bd10261de2c2e47a4dd317b40699
linjialiang <linjialiang@163.com>
2023-07-21 22:47:55 +0800

    update

--- .vitepress/sidebar/debian/debian.ts
+++ .vitepress/sidebar/debian/debian.ts
@@ -1,3 +1,5 @@
+import gui from './gui';
+
 const path = {
   main: '/debian/',
 };
@@ -6,7 +8,8 @@ const sidebar = [
   { text: '概述', link: `${path.main}` },
   { text: '安装操作系统', link: `${path.main}install_os` },
   { text: '配置操作系统', link: `${path.main}config_os` },
-  { text: 'xfce桌面', link: `${path.main}xfce` },
+  { text: 'gui', collapsed: true, items: [...gui] },
+  { text: 'GUI 桌面', link: `${path.main}gui` },
   { text: 'PhpStorm', link: `${path.main}phpstorm` },
 ];
 
---
+++ .vitepress/sidebar/debian/gui.ts
@@ -0,0 +1,10 @@
+const path = {
+  main: '/debian/gui/',
+};
+
+const sidebar = [
+  { text: 'GUI概述', link: `${path.main}` },
+  { text: 'PhpStorm', link: `${path.main}phpstorm` },
+];
+
+export default sidebar;
