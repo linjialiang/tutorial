9595838064be874e1771f1ba7520b8c13df2bc19
linjialiang <linjialiang@163.com>
2023-07-21 22:48:13 +0800

    update

--- .vitepress/sidebar/debian/debian.ts
+++ .vitepress/sidebar/debian/debian.ts
@@ -9,8 +9,6 @@ const sidebar = [
   { text: '安装操作系统', link: `${path.main}install_os` },
   { text: '配置操作系统', link: `${path.main}config_os` },
   { text: 'gui', collapsed: true, items: [...gui] },
-  { text: 'GUI 桌面', link: `${path.main}gui` },
-  { text: 'PhpStorm', link: `${path.main}phpstorm` },
 ];
 
 export default sidebar;
