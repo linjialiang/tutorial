4c46ad9cf25f7b5e8f2a19f41f0c192bbd057769
linjialiang <linjialiang@163.com>
2023-07-29 22:44:37 +0800

    update

--- .vitepress/sidebar/debian/debian.ts
+++ .vitepress/sidebar/debian/debian.ts
@@ -8,7 +8,6 @@ const sidebar = [
   { text: '概述', link: `${path.main}` },
   { text: '安装操作系统', link: `${path.main}install_os` },
   { text: '配置操作系统', link: `${path.main}config_os` },
-  { text: 'PHP环境', link: `${path.main}environment` },
   { text: 'gui', collapsed: true, items: [...gui] },
 ];
 
