b299e10387fa95b59654c8be78e92305a7dd35be
linjialiang <linjialiang@163.com>
2023-08-09 21:47:25 +0800

    update

--- assets/environment/source/bash/user.bash
+++ assets/environment/source/bash/user.bash
@@ -2,8 +2,8 @@
 
 WEB_USER='emad'
 WEB_USER_GROUP='emad'
-useradd -c 'developer user' -u 600 -s /bin/zsh ${WEB_USER}
-useradd -c 'This is the MySQL service user' -u 601 -s /usr/sbin/nologin mysql
-useradd -c 'redis service main process user' -u 602 -s /usr/sbin/nologin redis
-useradd -c 'nginx service work process user' -u 603 -s /usr/sbin/nologin nginx
-useradd -c 'php-fpm service work process user' -u 605 -s /usr/sbin/nologin phpfpm
+useradd -c 'developer user' -u 2000 -s /bin/zsh ${WEB_USER}
+useradd -c 'This is the MySQL service user' -u 2001 -s /usr/sbin/nologin mysql
+useradd -c 'redis service main process user' -u 2002 -s /usr/sbin/nologin redis
+useradd -c 'nginx service work process user' -u 2003 -s /usr/sbin/nologin nginx
+useradd -c 'php-fpm service work process user' -u 2005 -s /usr/sbin/nologin phpfpm
