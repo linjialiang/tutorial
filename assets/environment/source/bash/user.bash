91baa603075e4550c59b524282570ab98eeac206
linjialiang <linjialiang@163.com>
2024-02-03 13:43:38 +0800

    update

--- assets/environment/source/bash/user.bash
+++ assets/environment/source/bash/user.bash
@@ -3,7 +3,6 @@
 WEB_USER='emad'
 WEB_USER_GROUP='emad'
 useradd -c 'developer user' -u 2000 -s /bin/zsh ${WEB_USER}
-useradd -c 'redis service main process user' -u 2002 -s /usr/sbin/nologin redis
 useradd -c 'nginx service work process user' -u 2003 -s /usr/sbin/nologin nginx
 useradd -c 'php-fpm service work process user' -u 2005 -s /usr/sbin/nologin phpfpm
 
