f6bef3f632ccfa77d60261594a7d507e9760aa4e
linjialiang <linjialiang@163.com>
2024-02-03 13:48:15 +0800

    update

--- assets/environment/source/bash/user.bash
+++ assets/environment/source/bash/user.bash
@@ -7,13 +7,13 @@ useradd -c 'nginx service work process user' -u 2003 -s /usr/sbin/nologin nginx
 useradd -c 'php-fpm service work process user' -u 2005 -s /usr/sbin/nologin phpfpm
 
 # 创建 postgres 用户
-groupadd postgres
+groupadd -g 2001 postgres
 useradd -c 'postgres service main process user' -g postgres -u 2001 -s /sbin/nologin -m postgres
 cp -r /root/{.oh-my-zsh,.zshrc} /home/postgres
 chown postgres:postgres /home/postgres/{.oh-my-zsh,.zshrc}
 
 # 创建 redis 用户
-groupadd redis
+groupadd -g 2002 redis
 useradd -c 'redis service main process user' -g redis -u 2002 -s /sbin/nologin -m redis
 cp -r /root/{.oh-my-zsh,.zshrc} /home/redis
 chown redis:redis /home/redis/{.oh-my-zsh,.zshrc}
