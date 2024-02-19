576f80d3b6bb8630a59eb6b9b3630c8e8113f773
linjialiang <linjialiang@163.com>
2024-02-19 10:38:35 +0800

    update

--- assets/debian/apt/tx-server.bash
+++ assets/debian/apt/tx-server.bash
@@ -1,8 +1,5 @@
 # /etc/apt/sources.list
 
 deb http://mirrors.tencentyun.com/debian bookworm main
-deb-src http://mirrors.tencentyun.com/debian bookworm main
 deb http://mirrors.tencentyun.com/debian bookworm-updates main
-deb-src http://mirrors.tencentyun.com/debian bookworm-updates main
 deb http://mirrors.tencentyun.com/debian-security bookworm-security main
-deb-src http://mirrors.tencentyun.com/debian-security bookworm-security main
--- assets/debian/apt/zkd-server.bash
+++ assets/debian/apt/zkd-server.bash
@@ -1,8 +1,5 @@
 # /etc/apt/sources.list
 
 deb http://mirrors.ustc.edu.cn/debian/ bookworm main
-deb-src http://mirrors.ustc.edu.cn/debian/ bookworm main
 deb http://mirrors.ustc.edu.cn/debian/ bookworm-updates main
-deb-src http://mirrors.ustc.edu.cn/debian/ bookworm-updates main
 deb http://mirrors.ustc.edu.cn/debian-security/ bookworm-security main
-deb-src http://mirrors.ustc.edu.cn/debian-security/ bookworm-security main
