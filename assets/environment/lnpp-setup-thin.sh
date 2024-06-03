84270829404a487c343c11184a3affc8d263bb06
linjialiang <linjialiang@163.com>
2024-06-03 18:28:58 +0800

    update

--- assets/environment/lnpp-setup-thin.sh
+++ assets/environment/lnpp-setup-thin.sh
@@ -40,15 +40,11 @@ cleanOldData(){
 
 #创建单个用户
 createSingleUser(){
-  uid=$1
-  userName=$2
-  gid=$3
-  groupName=$4
+  userName=$1
+  groupName=$2
   echo_green "创建 $userName 用户"
-  groupadd -g $gid $userName
-  useradd -c "$userName service main process user" -g $groupName -u $uid -s /sbin/nologin -m $userName
-  cp -r /root/{.oh-my-zsh,.zshrc} /home/$userName
-  chown $userName:$groupName -R /home/$userName/{.oh-my-zsh,.zshrc}
+  groupadd -g $userName
+  useradd -c "$userName service main process user" -g $groupName -s /sbin/nologin -m $userName
 }
 
 #创建用户
@@ -56,12 +52,12 @@ createUser(){
   echo_yellow "=================================================================="
   echo_green "创建nginx、php-fpm、Postgres的进程用户"
   echo_yellow "=================================================================="
-  createSingleUser 2001 'nginx' 2001 'nginx'
-  createSingleUser 2002 'postgres' 2002 'postgres'
-  createSingleUser 2003 'php-fpm' 2003 'php-fpm'
+  createSingleUser 'nginx' 'nginx'
+  createSingleUser 'postgres' 'postgres'
+  createSingleUser 'php-fpm' 'php-fpm'
   echo ' '
   echo ' '
-  echo_yellow "==============================postgres  soft  nofile  65535===================================="
+  echo_yellow "=================================================================="
   echo_green "处理php-fpm的socket文件授权问题"
   echo_yellow "当 php-fpm 主进程非特权用户时，需要考虑socket文件权限问题："
   echo_yellow "nginx 如果是通过 sock 文件代理转发给 php-fpm，php-fpm 主进程创建\n sock 文件时需要确保 nginx 子进程用户有读写 sock 文件的权限"
@@ -75,21 +71,6 @@ createUser(){
   echo_green "这里采用方式1，如想采用方式2，注意同步修改php-fpm配置文件"
   echo_yellow "=================================================================="
   usermod -G nginx php-fpm
-}
-
-#开发用户追加权限
-devUserPower(){
-  devUserName=$1
-  echo_yellow "=================================================================="
-  echo_green "开发用户追加权限"
-  echo_yellow "web/php文件所属用户都是开发用户，所以nginx和php-fpm用户需要追加开发组"
-  echo_yellow " "
-  echo_red "部署环境请注释此函数，开发环境需要开启"
-  echo_red "部署环境不需要开发用户，可直接使用 nginx 用户作为 ftp、ssh 等上传工具的用户"
-  echo_yellow "=================================================================="
-  usermod -a -G $devUserName nginx
-  usermod -a -G $devUserName php-fpm
-  usermod -G nginx,php-fpm,postgres $devUserName
 }
 
 #安装依赖包
@@ -136,7 +117,6 @@ InstallSystemctlUnit(){
 modFilePower(){
   echo_yellow "=================================================================="
   echo_green "文件权限"
-  echo_green "通常来讲压缩包里含的权限是正确的，这里重新执行一遍，更加稳妥"
   echo_yellow "=================================================================="
   echo_green "nginx文件权限"
   chown nginx:nginx -R /server/{nginx,sites}
@@ -148,16 +128,13 @@ modFilePower(){
   chown nginx:nginx -R /server/run/nginx
   chmod 750 /server/run/nginx
 
-  echo_green "为nginx启用CAP_NET_BIND_SERVICE能力"
-  echo_red "注：每次修改nginx执行文件权限，都需要重新启用该能力"
-  setcap cap_net_bind_service=+eip /server/nginx/sbin/nginx
-
   echo_green "php文件权限"
   chown php-fpm:php-fpm -R /server/php /server/logs/php /server/run/php
   find /server/php /server/logs/php -type f -exec chmod 640 {} \;
   find /server/php /server/logs/php -type d -exec chmod 750 {} \;
   chmod 750 -R /server/php/83/bin /server/php/83/sbin
   chmod 755 /server/run/php
+
   echo_green "postgres文件权限"
   chown postgres:postgres -R /server/postgres /server/pgData /server/logs/postgres /server/run/postgres
   find /server/postgres /server/logs/postgres /server/run/postgres -type f -exec chmod 640 {} \;
@@ -165,6 +142,10 @@ modFilePower(){
   find /server/pgData /server/postgres/tls -type f -exec chmod 600 {} \;
   find /server/pgData -type d -exec chmod 700 {} \;
   chmod 750 -R /server/postgres/bin
+
+  echo_green "为nginx启用CAP_NET_BIND_SERVICE能力"
+  echo_red "注：每次修改nginx执行文件权限，都需要重新启用该能力"
+  setcap cap_net_bind_service=+eip /server/nginx/sbin/nginx
 }
 
 #清理旧数据
@@ -174,11 +155,6 @@ echo ' '
 
 #创建用户
 createUser
-echo ' '
-echo ' '
-
-#开发用户追加权限，部署环境请注释掉，emad是开发用户名
-devUserPower 'emad'
 echo ' '
 echo ' '
 
@@ -203,12 +179,22 @@ echo ' '
 echo ' '
 
 echo_yellow "=================================================================="
-echo_green "针对 Postgres用户 修改操作系统打开最大文件句柄数"
+echo_green "针对 Postgres用户 修改操作系统打开最大文件句柄数\n防止linux操作系统内打开文件句柄数量的限制，避免不必要的故障"
 echo_yellow "为防止重复插入，请在 /etc/security/limits.conf 文件的结尾手动添加\n如下两行代码："
 echo_red "注：这里是手动操作哦！！！"
 echo_yellow " "
 echo_cyan "postgres  soft  nofile  65535"
 echo_cyan "postgres  hard  nofile  65535"
 echo_yellow " "
-echo_green "进行这一步操作的目的是防止linux操作系统内打开文件句柄数量的限制，\n避免不必要的故障"
+echo_yellow "=================================================================="
+echo ' '
+echo ' '
+
+echo_yellow "=================================================================="
+echo_green "精简版不含开发者用户的权限授予，如有需要请自行授权："
+echo_yellow " "
+echo_cyan "usermod -a -G 开发组名 nginx"
+echo_cyan "usermod -a -G 开发组名 php-fpm"
+echo_cyan "usermod -G nginx,php-fpm,postgres 开发用户名"
+echo_yellow " "
 echo_yellow "=================================================================="
