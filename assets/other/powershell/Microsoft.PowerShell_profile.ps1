5cc46ff48aafe32e198b4596132ab9cbf80137d5
linjialiang <linjialiang@163.com>
2024-11-11 16:24:49 +0800

    update

--- assets/other/powershell/Microsoft.PowerShell_profile.ps1
+++ assets/other/powershell/Microsoft.PowerShell_profile.ps1
@@ -5,8 +5,8 @@ Import-Module posh-git
 # 命令预测的来源设为历史记录
 Set-PSReadLineOption -PredictionSource History
 # 设置编辑器模式为Vi模式:
-Set-PSReadlineOption -EditMode Vi
+# Set-PSReadlineOption -EditMode Vi
 # 如果想区分两种模式的光标
-Set-PSReadlineOption -ViModeIndicator Cursor
+# Set-PSReadlineOption -ViModeIndicator Cursor
 # Tab 按键兼容 oh-my-posh 和 Vi 模式
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
