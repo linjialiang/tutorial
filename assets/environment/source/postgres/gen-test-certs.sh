4b760f080235a773d488f7ee0346557a093fa99e
linjialiang <linjialiang@163.com>
2024-12-12 10:55:38 +0800

    update

--- assets/environment/source/postgres/gen-test-certs.sh
+++ assets/environment/source/postgres/gen-test-certs.sh
@@ -52,7 +52,8 @@ nsCertType = client
 _END_
 
 generate_cert server "Server-only" "-extfile tls/openssl.cnf -extensions server_cert"
-generate_cert client-admin "Client-only" "-extfile tls/openssl.cnf -extensions client_cert"
+generate_cert client-admin "admin" "-extfile tls/openssl.cnf -extensions client_cert"
+generate_cert client-emad "emad" "-extfile tls/openssl.cnf -extensions client_cert"
 generate_cert pgsql "Generic-cert"
 
 [ -f tls/pgsql.dh ] || openssl dhparam -out tls/pgsql.dh 2048
