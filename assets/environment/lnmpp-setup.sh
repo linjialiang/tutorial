8b269b17a3d079a39b38fdd6b64e1af2060c8eb7
linjialiang <linjialiang@163.com>
2024-12-12 13:34:00 +0800

    update

--- assets/environment/lnmpp-setup.sh
+++ assets/environment/lnmpp-setup.sh
@@ -238,7 +238,56 @@ resetPgsqlCertificate(){
   echo_cyan "[+] Create PostgreSQL certs script..."
   tlsScriptPath=/server/postgres/gen-test-certs.sh
   echo "#\!/bin/bash
+generate_cert() {
+    local name=\$1
+    local cn=\"\$2\"
+    local opts=\"\$3\"
 
+    local keyfile=tls/\${name}.key
+    local certfile=tls/\${name}.crt
+
+    [ -f \$keyfile ] || openssl genrsa -out \$keyfile 2048
+    openssl req \\
+        -new -sha256 \\
+        -subj \"/O=PostgreSQL Test/CN=\$cn\" \\
+        -key \$keyfile | \\
+        openssl x509 \\
+            -req -sha256 \\
+            -CA tls/root.crt \\
+            -CAkey tls/root.key \\
+            -CAserial tls/root.txt \\
+            -CAcreateserial \\
+            -days 365 \\
+            \$opts \\
+            -out \$certfile
+}
+
+mkdir tls
+[ -f tls/root.key ] || openssl genrsa -out tls/root.key 4096
+openssl req \\
+    -x509 -new -nodes -sha256 \\
+    -key tls/root.key \\
+    -days 3650 \\
+    -subj '/O=PostgreSQL Test/CN=Certificate Authority' \\
+    -out tls/root.crt
+
+cat > tls/openssl.cnf <<_END_
+[ server_cert ]
+keyUsage = digitalSignature, keyEncipherment
+nsCertType = server
+
+[ client_cert ]
+keyUsage = digitalSignature, keyEncipherment
+nsCertType = client
+_END_
+
+generate_cert server \"Server-only\" \"-extfile tls/openssl.cnf -extensions server_cert\"
+generate_cert client \"Client-only\" \"-extfile tls/openssl.cnf -extensions client_cert\"
+generate_cert client-admin \"admin\" \"-extfile tls/openssl.cnf -extensions client_cert\"
+generate_cert client-emad \"emad\" \"-extfile tls/openssl.cnf -extensions client_cert\"
+generate_cert pgsql \"Generic-cert\"
+
+[ -f tls/pgsql.dh ] || openssl dhparam -out tls/pgsql.dh 2048
 " > $tlsScriptPath
   chmod +x $tlsScriptPath
   $tlsScriptPath
