d81b12ba49b2a222db8d3bee4f978f2598cfe256
linjialiang <linjialiang@163.com>
2025-09-19 17:11:24 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,12 +2,13 @@
 import 'viewerjs/dist/viewer.min.css'; // 引入 viewerjs 的样式[1](@ref)
 import { useRoute } from 'vitepress';
 import imageViewer from 'vitepress-plugin-image-viewer';
+import Theme from 'vitepress/theme';
 import Layout from './Layout.vue';
 import './style.css';
 import './styles/custom.css';
 
 export default {
-  // extends: Theme,
+  extends: Theme,
   Layout,
   setup() {
     const route = useRoute();
